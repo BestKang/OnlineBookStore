090a99c27b9ab1a07e6f46808caabd16557a0052
BestKang <34906546+BestKang@users.noreply.github.com>
2018-07-12 08:58:47 +0800

    大大大

--- src/dao/DbMethod.java
+++ src/dao/DbMethod.java
@@ -493,4 +493,24 @@ public class DbMethod {
 			e.printStackTrace();
 		}
 	}
+	public Connection getConnection(){
+		return conn;
+		
+	}
+	public void setAutoCommit(){
+		try {
+			conn.setAutoCommit(false);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void commit(){
+		try {
+			conn.commit();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
